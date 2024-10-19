89ef3d718dace7d6a6c87d82080f3cd050ef9821
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-20 03:52:29 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Input {
+    public static void main(String args[]){
+        Scanner sc =  new Scanner(System.in);
+
+      //  String input = sc.next();
+     //   System.out.print(input);
+
+        String name = sc.nextLine();
+        System.out.print(name);
+    }
+}
