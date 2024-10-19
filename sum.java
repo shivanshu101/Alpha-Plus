c90a9fefab4f0fb4989217cc5ad20de1b598ce79
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-20 03:52:48 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.lang.constant.DynamicConstantDesc;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
