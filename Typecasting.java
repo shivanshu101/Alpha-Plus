970e1adf8179b4482cef1881e76ce6a9a6eb3dcf
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-22 04:34:06 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,11 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch2 = 'b';  // 'b', 'd', e
+        int number = ch;
+        int  number2 = ch2;
+        System.out.println(number);
+        System.out.println(number2);
+
+    }
+}
