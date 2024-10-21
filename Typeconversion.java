5b8660097b3dffe06280b22b54026dae97b6a7e1
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-22 04:33:34 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Typeconversion {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        float number =  sc.nextInt();
+        System.out.println(number);
+    }
+}
