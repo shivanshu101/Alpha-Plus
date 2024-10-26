56e6b27674f8e2219834147bfc37a675ec02eb06
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-27 01:13:24 +0530

    Add files via upload

---
+++ Unarypre.java
@@ -0,0 +1,30 @@
+public class Unarypre {
+    public static void main(String[] args){
+       //pre  increment
+
+        // int a = 10;    
+       //int b = ++a;
+       // System.out.println(a);
+      //  System.out.println(b);
+
+      // post increment
+
+      // int a = 10;
+       // int b = a++;
+       // System.out.println(a);
+       // System.out.println(b);
+
+        //pre Decrement
+       // int a = 10;  
+       // int b = --a;
+       // System.out.println(a);
+      //System.out.println(b);
+
+        // post Decrement
+
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
