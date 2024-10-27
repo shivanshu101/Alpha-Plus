1b6bb73ff72c5359aa3671bf23821c74f2643c76
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-28 01:25:10 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,15 @@
+public class Ifelse {
+    public  static void main(String[] args) {
+   int age = 16;
+   if(age >= 18) {
+    System.out.println("You are eligible for voting and driving");
+   }
+   if(age > 13 && age < 18){
+    System.out.println("teenager");
+   }
+   else {
+    System.out.println("You are not eligible for voting and driving");
+}
+    }
+
+}
