585647298e2c6b5f8e08f134e1096713e3f65acb
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-29 01:36:02 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String[] args) {
+        int marks = 67;
+        String reportCard = marks >= 33 ?  "Pass" : "Fail";
+        System.out.println(reportCard);
+
+    }
+}
