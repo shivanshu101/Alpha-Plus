62cdfe276e9551c6c41203121e1288d13982474a
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-30 01:32:52 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Switch {
+    public static void main(String[] args) {
+        char ch  = 'a';
+
+        switch(ch) {
+            case 'b': System.out.println("samosa");
+                        break;
+            case 'a': System.out.println("burger");
+                        break;
+            case 'c': System.out.println("manga shake");
+                        break;
+            default : System.out.println("wake up");
+
+
+        }
+    
+
+    }
+}
