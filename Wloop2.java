9542156b55623b9323f84166c44771ed135d531e
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-31 02:38:38 +0530

    Add files via upload

---
+++ Wloop2.java
@@ -0,0 +1,9 @@
+public class Wloop2 {
+    public static void main(String[] args) {
+        int sum = 1;
+        while(sum <= 5){
+            System.out.println(sum);
+            sum++;
+        }
+    }
+}
