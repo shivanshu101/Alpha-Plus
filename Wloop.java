bca2ec1cd1e20c69939bc1f7c378fb15d98b9f0c
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-31 02:38:13 +0530

    Add files via upload

---
+++ Wloop.java
@@ -0,0 +1,10 @@
+public class Wloop {
+    public static void main(String[] args) {
+        int number = 1;
+        while(number  <= 100) {
+            System.out.print(number + " ");
+            number++;
+    }
+    System.out.println();
+    }
+}
