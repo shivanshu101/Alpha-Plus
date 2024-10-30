f05cb421929f1ad60e42f0b8994f8ea4035c4280
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-31 02:37:38 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("hello world");
+            counter++;
+        }
+            System.out.println("print hello world 10 times");
+    }
+}
