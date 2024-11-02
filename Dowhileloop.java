651ff5214922f001ad6c0f2539eb798408ce707c
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-11-03 01:28:54 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,9 @@
+public class Dowhileloop {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("hello world");
+            counter++;
+        } while(counter <= 10);
+    }
+}
