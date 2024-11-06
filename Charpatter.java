c93e521ec692dae807b8392e596b35ebbd868d2d
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-11-07 00:50:49 +0530

    Add files via upload

---
+++ Charpatter.java
@@ -0,0 +1,15 @@
+public class Charpatter {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        // outer loop
+        for(int line = 1; line <= 4; line++){
+            // inner  loop
+            for(int chars  = 1; chars <= line; chars++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
